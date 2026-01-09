df8507eb8aacaaed9c264445edcca7723a4bd6d3
afk.puma <afk.puma@gmail.com>
2026-01-08 21:00:47 -0300

    feat: adiciona rota para simular recebimento de mensagens do WhatsApp

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class MensagemWhatsApp(BaseModel):
+    telefone: str
+    mensagem: str
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"mensagem": "API do bot de ar-condicionado funcionando"}
+
+@app.post("/whatsapp")
+def receber_mensagem(dados: MensagemWhatsApp):
+    return {
+        "telefone": dados.telefone,
+        "resposta": f"Recebi sua mensagem: {dados.mensagem}"
+    }
